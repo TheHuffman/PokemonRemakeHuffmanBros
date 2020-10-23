cb1f4e3e10df5a77e91302a2f9a2bbf6dd4957ee
huffmag <huffmag@wwu.edu>
2020-10-23 14:42:50 -0700

    Removing the old src code

--- src/main/java/com/NotGarbage/engine/AbstractGame.java
+++
@@ -1,7 +0,0 @@
-package com.NotGarbage.engine;
-
-public abstract class AbstractGame {
-
-	public abstract void update(GameContainer gc, float dt);
-	public abstract void render(GameContainer gc, Renderer r);
-}
